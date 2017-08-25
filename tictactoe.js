b1d16ce76325035c7f4942e95e45435248ca3499
LONEWOLFSW\jtollette <jtollette@lwolf.com>
2017-08-25 11:24:59 -0500

    syntax error fixed

--- tictactoe.js
+++ tictactoe.js
@@ -137,9 +137,7 @@ function calculateWinner(squares) {
     const [a,b,c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
       return squares[a];
-    } else{
-      return 
-    }
+    } 
   }
   return null;
 }
